f9ac24e787092a5e0a8783c40f37e5c91a0b4e0d
Magdalena Falkowska-Legieć <magda.f41@gmail.com>
2020-08-10 19:28:26 +0200

    zadanie_36 - poprawione

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
-let num1, num2;
-
 function number(num1, num2) {
-    if (num1 == 20 || num2 == 20) {
-        return console.log("true");
-    } else if (num1 + num2 <= 20) {
-        return console.log("true");
+    if (num1 == 20 || num2 == 20 || num1 + num2 <= 20) {
+        return true;
     } else {
-        return console.log("Żaden warunek nie został spełniony");
-
+        return "Żaden warunek nie został spełniony";
     }
 }
-number(5, 10);
+console.log(number(5, 10));
 
